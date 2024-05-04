60ec916c1d32c5b4dd5c6e2ec5a2e566be6aa74e
shandywi <80673125+shandywi@users.noreply.github.com>
2024-05-04 21:37:41 +0700

    add jupiter
    adding new planet to explore

--- helloApi/Program.cs
+++ helloApi/Program.cs
@@ -5,6 +5,7 @@ app.MapGet("/", () => "Hi Human, please type the name of your destination planet
 app.MapGet("/merkurius", () => "You are landing on merkurius!");
 app.MapGet("/venus", () => "You are landing on venus!");
 app.MapGet("/earth", () => "You are landing on earth!");
+app.MapGet("/jupiter", () => "You are landing on jupiter!");
 
 if (!app.Environment.IsDevelopment())
 {
