9125b6fcc4220927399f88c4812e3b80b79c8a08
shandywi <80673125+shandywi@users.noreply.github.com>
2024-05-08 14:26:24 +0700

    add new planet

--- helloApi/Program.cs
+++ helloApi/Program.cs
@@ -6,6 +6,8 @@ app.MapGet("/merkurius", () => "You are landing on merkurius!");
 app.MapGet("/venus", () => "You are landing on venus!");
 app.MapGet("/earth", () => "You are landing on earth!");
 app.MapGet("/jupiter", () => "You are landing on jupiter!");
+app.MapGet("/mars", () => "You are landing on mars!");
+
 
 if (!app.Environment.IsDevelopment())
 {
